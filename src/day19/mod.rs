0ebde57ebdfc2f6ab253604e260e80bfaa98cfe6
Florian Loch <me@fdlo.ch>
2022-12-19 19:22:38 +0100

    refactor: rework day 19 in order to solve part 2, became much, much faster

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,4 +1,5 @@
-use std::cmp::max;
+use std::cmp::{max, min};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
@@ -9,22 +10,43 @@ pub fn solve(lines: &mut Lines<BufReader<File>>, test: bool) {
         Blueprint::from_text(&line.expect("Failed to read line"))
     }).collect();
 
+    println!("===> Part 1");
+
     let mut sum_quality_levels = 0;
 
     for (idx, blueprint) in blueprints.iter().enumerate() {
-        let geode_production = blueprint.simulate(24, &Robots{
+        let geode_production = blueprint.simulate(24, Robots{
             ore_robots: 1,
             clay_robots: 0,
             obsidian_robots: 0,
             geode_robots: 0,
-        }, 0, 0, 0, 0);
+        }, 0, 0, 0, 0, 0);
 
         println!("Blueprint {}: Geode production: {}", idx + 1, geode_production);
 
-        sum_quality_levels += (idx + 1) * geode_production as usize;
+        sum_quality_levels += (idx as i32 + 1) * geode_production;
     }
 
-    println!("Part 1: Sum of blueprints' quality levels: {}", sum_quality_levels);
+    println!("Sum of blueprints' quality levels: {}", sum_quality_levels);
+
+    println!("===> Part 2");
+
+    let mut prod_quality_levels = 1;
+
+    for i in 0..min(3, blueprints.len()) {
+        let geode_production = blueprints[i].simulate(32, Robots {
+            ore_robots: 1,
+            clay_robots: 0,
+            obsidian_robots: 0,
+            geode_robots: 0,
+        }, 0, 0, 0, 0, 0);
+
+        println!("Blueprint {}: Geode production: {}", i + 1, geode_production);
+
+        prod_quality_levels *= geode_production;
+    }
+
+    println!("Product of blueprints' productions: {}", prod_quality_levels);
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -71,62 +93,85 @@ impl Blueprint {
         };
     }
 
-    fn simulate(&self, mut minutes_remaining: usize, robots_available: &Robots, ore_available: i32, clay_available: i32, obsidian_available: i32, geodes_collected: i32) -> i32 {
-        // ore_available += robots_available.ore_robots;
-        // clay_available += robots_available.clay_robots;
-        // obsidian_available += robots_available.obsidian_robots;
-        // geodes_collected += robots_available.geode_robots;
-
+    fn simulate(&self, mut minutes_remaining: i32, robots_available: Robots, ore_available: i32, clay_available: i32, obsidian_available: i32, geodes_collected: i32, mut max_geode_production_so_far: i32) -> i32 {
         if minutes_remaining <= 1 || (minutes_remaining <= 2 && robots_available.geode_robots == 0 && (ore_available < self.costs_geode_robot.ore || obsidian_available < self.costs_geode_robot.obsidian)) {
-            return geodes_collected + minutes_remaining as i32 * robots_available.geode_robots;
+            return geodes_collected + minutes_remaining * robots_available.geode_robots;
         }
 
         if minutes_remaining == 1 {
             return geodes_collected + robots_available.geode_robots;
         }
 
+        let genereous_prediction = geodes_collected + self.predict_generously(minutes_remaining, robots_available, obsidian_available);
+
+        if genereous_prediction <= max_geode_production_so_far {
+            return -1;
+        }
+
         minutes_remaining -= 1;
 
-        let mut max_geode_production = self.simulate(minutes_remaining, robots_available, ore_available+robots_available.ore_robots, clay_available+robots_available.clay_robots, obsidian_available+robots_available.obsidian_robots, geodes_collected+robots_available.geode_robots);
+        let mut max_geode_production = 0;
 
         if ore_available >= self.costs_ore_robot.ore {
-            let mut robots_available_next_minute = *robots_available;
+            let mut robots_available_next_minute = robots_available;
 
             robots_available_next_minute.ore_robots += 1;
 
-            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, &robots_available_next_minute, ore_available+robots_available.ore_robots-self.costs_ore_robot.ore, clay_available+robots_available.clay_robots, obsidian_available+robots_available.obsidian_robots, geodes_collected+robots_available.geode_robots));
+            max_geode_production = self.simulate(minutes_remaining, robots_available_next_minute, ore_available + robots_available.ore_robots - self.costs_ore_robot.ore, clay_available + robots_available.clay_robots, obsidian_available + robots_available.obsidian_robots, geodes_collected + robots_available.geode_robots, max_geode_production_so_far);
         }
 
         if ore_available >= self.costs_clay_robot.ore {
-            let mut robots_available_next_minute = *robots_available;
+            let mut robots_available_next_minute = robots_available;
 
             robots_available_next_minute.clay_robots += 1;
 
-            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, &robots_available_next_minute, ore_available+robots_available.ore_robots-self.costs_clay_robot.ore, clay_available+robots_available.clay_robots, obsidian_available+robots_available.obsidian_robots, geodes_collected+robots_available.geode_robots));
+            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, robots_available_next_minute, ore_available + robots_available.ore_robots - self.costs_clay_robot.ore, clay_available + robots_available.clay_robots, obsidian_available + robots_available.obsidian_robots, geodes_collected + robots_available.geode_robots, max(max_geode_production_so_far, max_geode_production)));
         }
 
-        // if ore_available >= self.costs_obsidian_robot.ore && clay_available >= self.costs_obsidian_robot.clay && minutes_remaining as i32 - 2 > self.costs_geode_robot.obsidian {
         if ore_available >= self.costs_obsidian_robot.ore && clay_available >= self.costs_obsidian_robot.clay {
-            let mut robots_available_next_minute = *robots_available;
+            let mut robots_available_next_minute = robots_available;
 
             robots_available_next_minute.obsidian_robots += 1;
 
-            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, &robots_available_next_minute, ore_available+robots_available.ore_robots-self.costs_obsidian_robot.ore, clay_available+robots_available.clay_robots-self.costs_obsidian_robot.clay, obsidian_available+robots_available.obsidian_robots, geodes_collected+robots_available.geode_robots));
+            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, robots_available_next_minute, ore_available + robots_available.ore_robots - self.costs_obsidian_robot.ore, clay_available + robots_available.clay_robots - self.costs_obsidian_robot.clay, obsidian_available + robots_available.obsidian_robots, geodes_collected + robots_available.geode_robots, max(max_geode_production_so_far, max_geode_production)));
         }
 
         if ore_available >= self.costs_geode_robot.ore && obsidian_available >= self.costs_geode_robot.obsidian {
-            let mut robots_available_next_minute = *robots_available;
+            let mut robots_available_next_minute = robots_available;
 
             robots_available_next_minute.geode_robots += 1;
 
-            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, &robots_available_next_minute, ore_available+robots_available.ore_robots-self.costs_geode_robot.ore, clay_available+robots_available.clay_robots, obsidian_available+robots_available.obsidian_robots-self.costs_geode_robot.obsidian, geodes_collected+robots_available.geode_robots));
+            max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, robots_available_next_minute, ore_available + robots_available.ore_robots - self.costs_geode_robot.ore, clay_available + robots_available.clay_robots, obsidian_available + robots_available.obsidian_robots - self.costs_geode_robot.obsidian, geodes_collected + robots_available.geode_robots, max(max_geode_production_so_far, max_geode_production)));
         }
+
+        max_geode_production = max(max_geode_production, self.simulate(minutes_remaining, robots_available, ore_available + robots_available.ore_robots, clay_available + robots_available.clay_robots, obsidian_available + robots_available.obsidian_robots, geodes_collected + robots_available.geode_robots, max(max_geode_production_so_far, max_geode_production)));
 
         return max_geode_production;
     }
+
+    fn predict_generously(&self, minutes_remaining: i32, mut robots_available: Robots, mut obsidian_available: i32) -> i32 {
+        let mut max_geode_gain = 0;
+
+        // Let's assume, we could add another obsidian robot every minute
+        for _ in 0..minutes_remaining {
+            obsidian_available += robots_available.obsidian_robots;
+            robots_available.obsidian_robots += 1;
+
+            max_geode_gain += robots_available.geode_robots;
+
+            if obsidian_available >= self.costs_geode_robot.obsidian {
+                obsidian_available -= self.costs_geode_robot.obsidian;
+
+                robots_available.geode_robots += 1;
+            }
+        }
+
+        return max_geode_gain;
+    }
 }
 
-#[derive(Debug, Copy, Clone)]
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 struct Robots {
     ore_robots: i32,
     clay_robots: i32,
